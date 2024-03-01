9ed798dd5d04a5a04951bdc2e674ef8bff7afe7d
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-03-01 10:18:15 +0530

    GoldenRatio - fix animation

--- src/main/kotlin/com/faangx/ktp/basics/loops/GoldenRatio.kt
+++ src/main/kotlin/com/faangx/ktp/basics/loops/GoldenRatio.kt
@@ -1,22 +1,19 @@
 package com.faangx.ktp.basics.loops
 
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.ui.geometry.Size
+import androidx.compose.animation.core.keyframes
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.inset
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
@@ -37,9 +34,9 @@ fun goldenRatioApp() = application {
         height = 818.dp
     )
 
-//    LaunchedEffect(state.size) {
-//        println("size = (${state.size})")
-//    }
+    LaunchedEffect(state.size) {
+        println("size = (${state.size})")
+    }
 
     Window(
         onCloseRequest = ::exitApplication,
@@ -78,112 +75,86 @@ enum class CircleCenter {
 
 @Composable
 fun GoldenRatioSpiral() {
-    var scale by remember { mutableStateOf(300f) }
-    val animatedScale = animateFloatAsState(scale)
-
+    val scale = 10f
+//    val list = listOf(1) + (1..70).toList()
     val list = listOf(1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597)
     var pointer = Offset(0f, 0f)
     var direction = Left
     var prevNum = list.first()
     var angle = 0f
 
-    var rotateDegree by remember { mutableStateOf(0f) }
+    var mainScale by remember { mutableStateOf(10f) }
 
-    LaunchedEffect(rotateDegree) {
-        delay(10)
-        rotateDegree -= 0.1f
-    }
+    LaunchedEffect(Unit) { delay(1); mainScale = 0.25f }
 
-    LaunchedEffect(scale) {
-        delay((200 - scale).toLong())
-        if (scale != 2f) scale -= 1f
-    }
-
-    Box (
-        modifier = Modifier.fillMaxSize()
-    ) {
-
-        Canvas(
-            modifier = Modifier.fillMaxSize()
-                .background(Color(0xff570bb7))
-        ) {
-            rotate(rotateDegree) {
-                inset(left = size.width / 2f, top = size.height / 2f, right = 0f, bottom = 0f) {
-
-                    list.forEachIndexed { index, num ->
-                        if (index != 0) {
-                            pointer = when (direction) {
-                                Left -> Offset(pointer.x - num, pointer.y)
-                                Down -> Offset(pointer.x, pointer.y + prevNum)
-                                Right -> Offset(pointer.x + prevNum, pointer.y - (num - prevNum))
-                                Up -> Offset(pointer.x - (num - prevNum), pointer.y - num)
-                            }
-
-                            direction = direction.next()
-                            prevNum = num
-                        }
-
-                        val size = Size(num * animatedScale.value, num * animatedScale.value)
-
-                        drawRect(
-                            color = Color.White,
-                            size = size,
-                            topLeft = pointer.scale(animatedScale.value),
-                            style = Stroke(width = 0.2f)
-                        )
-
-                        drawArc(
-                            color = Color.White,
-                            startAngle = angle,
-                            sweepAngle = -90f,
-                            useCenter = false,
-                            topLeft = arcOffsetForCircleCenter(
-                                Companion.forDirection(direction),
-                                pointer.scale(animatedScale.value),
-                                num * animatedScale.value
-                            ),
-                            size = Size(size.width * 2, size.height * 2),
-                            style = Stroke(4f)
-                        )
-
-                        angle -= 90
-                    }
-                }
+    val animatedMainScale = animateFloatAsState(
+        mainScale,
+        animationSpec = keyframes {
+            durationMillis = 61000
+            var delay = 700f
+            for (i in (9 downTo 1).toList() + listOf(0.75f, 0.5f, 0.25f)) {
+                i.toFloat() at delay.toInt()
+                println(delay)
+                delay *= 1.5f
             }
         }
+    )
 
-        Row(
-            modifier = Modifier.padding(36.dp)
-                .align(Alignment.BottomEnd)
-        ) {
-            IconButton(
-                onClick = {
-                    if (scale != 2f) scale -= 5f
+    var rotation by remember { mutableStateOf(0f) }
+    val animatedRotation = animateFloatAsState(rotation)
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(100)
+            rotation -= 1
+        }
+    }
+
+    Canvas(
+        modifier = Modifier.fillMaxSize()
+            .background(Color(0xff570bb7))
+//            .scale(mainScale)
+            .scale(animatedMainScale.value)
+            .rotate(animatedRotation.value)
+    ) {
+        inset(left = size.width / 2f, top = size.height / 2f - 200f, right = 0f, bottom = 0f) {
+
+            list.forEachIndexed { index, num ->
+                if (index != 0) {
+                    pointer = when (direction) {
+                        Left -> Offset(pointer.x - num, pointer.y)
+                        Down -> Offset(pointer.x, pointer.y + prevNum)
+                        Right -> Offset(pointer.x + prevNum, pointer.y - (num - prevNum))
+                        Up -> Offset(pointer.x - (num - prevNum), pointer.y - num)
+                    }
+
+                    direction = direction.next()
+                    prevNum = num
                 }
-            ) {
-                Text(
-                    text = "-",
-                    color = Color.White,
-                    style = MaterialTheme.typography.h3
-                )
-            }
 
-            Text(
-                modifier = Modifier.width(100.dp),
-                text = "${scale.toInt()}",
-                color = Color.White,
-                style = MaterialTheme.typography.h3,
-                textAlign = TextAlign.Center
-            )
+                val size = Size(num * scale, num * scale)
 
-            IconButton(
-                onClick = { scale += 5f }
-            ) {
-                Text(
-                    text = "+",
+                drawRect(
                     color = Color.White,
-                    style = MaterialTheme.typography.h3
+                    size = size,
+                    topLeft = pointer.scale(scale),
+                    style = Stroke(width = 0.2f)
                 )
+
+                drawArc(
+                    color = Color.White,
+                    startAngle = angle,
+                    sweepAngle = -90f,
+                    useCenter = false,
+                    topLeft = arcOffsetForCircleCenter(
+                        Companion.forDirection(direction),
+                        pointer.scale(scale),
+                        num * scale
+                    ),
+                    size = Size(size.width * 2, size.height * 2),
+                    style = Stroke(4f)
+                )
+
+                angle -= 90
             }
         }
     }
