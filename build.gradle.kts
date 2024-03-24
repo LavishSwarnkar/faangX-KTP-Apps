cd38f464ddfba9f3f50718a7b7c65ed3b9e05966
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-03-24 19:26:39 +0530

    update artifactId

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.faangx.ktp"
-            artifactId = "mini-apps"
+            artifactId = "faangX-KTP-Apps"
             version = "1.0"
 
             from(components["java"])
