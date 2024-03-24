e1b3b9223a4601ca2d7c467311eae4c591d1c2fe
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-03-24 19:14:39 +0530

    refactor

--- src/main/kotlin/com/faangx/ktp/catalog/MiniAppsAggregator.kt
+++
@@ -1,9 +0,0 @@
-package com.faangx.ktp.catalog
-
-fun MiniApp.Companion.groupedByCategory(): Map<String, List<MiniApp>> {
-    return MiniApp.entries.groupBy { it.category }
-}
-
-fun List<MiniApp>.groupedBySubcategory(): Map<String, List<MiniApp>> {
-    return groupBy { it.subcategory ?: "NA" }
-}
