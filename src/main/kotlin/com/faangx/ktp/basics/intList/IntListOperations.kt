9254eb83f37d26f67c3b54e2a75f763336359b75
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-05-07 11:48:28 +0530

    split UpdateOp : InPlace, NewList

--- src/main/kotlin/com/faangx/ktp/basics/intList/IntListOperations.kt
+++ src/main/kotlin/com/faangx/ktp/basics/intList/IntListOperations.kt
@@ -60,7 +60,7 @@ fun IntListOperations(
             when (variant) {
                 is IntListOpsVariant.All -> {
 
-                    val selection: MutableState<IntListOp?> = remember { mutableStateOf(variant.ops[9]) }
+                    val selection: MutableState<IntListOp?> = remember { mutableStateOf(variant.ops[3]) }
                     val labelState = remember {
                         mutableStateOf(
                             TextInputState("Operation")
--- src/main/kotlin/com/faangx/ktp/basics/intList/op/UpdateOp.kt
+++ src/main/kotlin/com/faangx/ktp/basics/intList/op/UpdateOp.kt
@@ -14,11 +14,20 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.faangx.ktp.basics.intList.model.IntListOp
 
-class UpdateOp(
-    val update: (MutableList<Int>, Int, Int) -> Unit
-): IntListOp {
-    override val label: String = "Update Element"
-    override val Composable: @Composable (MutableList<Int>) -> Unit = { Comp(it) }
+interface UpdateOp: IntListOp {
+    class InPlace(
+        val update: (MutableList<Int>, Int, Int) -> Unit
+    ): UpdateOp {
+        override val label: String = "Update Element (In Place)"
+        override val Composable: @Composable (MutableList<Int>) -> Unit = { Comp(it) }
+    }
+
+    class NewList(
+        val update: (List<Int>, Int, Int) -> List<Int>
+    ): UpdateOp {
+        override val label: String = "Update Element (New List)"
+        override val Composable: @Composable (MutableList<Int>) -> Unit = { Comp(it) }
+    }
 }
 
 @Composable
@@ -63,7 +72,17 @@ fun UpdateOp.Comp(list: MutableList<Int>) {
                 val indexInt = index.toIntOrNull() ?: return@Button
                 val valueInt = value.toIntOrNull() ?: return@Button
 
-                update(list, indexInt, valueInt)
+                when (this@Comp) {
+                    is UpdateOp.InPlace -> {
+                        update(list, indexInt, valueInt)
+                    }
+                    is UpdateOp.NewList -> {
+                        val newList = update(list, indexInt, valueInt)
+                        list.clear()
+                        list.addAll(newList)
+                    }
+                }
+
             }
         ) {
             Text("SET")
