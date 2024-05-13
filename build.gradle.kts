6e2652e6231352f5869ca61bf8196087298002b4
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-05-13 17:47:13 +0530

    upgrade DroidLibs

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,8 @@ dependencies {
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
     implementation("org.jetbrains.compose.material3:material3-desktop:1.6.0")
-    implementation("com.github.The-Streamliners.DroidLibs:compose:1.2.4")
-    implementation("com.github.The-Streamliners.DroidLibs:utils:1.2.4")
+    implementation("com.github.The-Streamliners.DroidLibs:compose:1.2.14")
+    implementation("com.github.The-Streamliners.DroidLibs:utils:1.2.14")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.3")
 }
