f077c51589b6afbb3b2d040df451fa743667dcf2
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-05-15 18:54:01 +0530

    adaptiveLayout : StageOfLifeApp

--- src/main/kotlin/com/faangx/ktp/basics/StageOfLifeApp.kt
+++ src/main/kotlin/com/faangx/ktp/basics/StageOfLifeApp.kt
@@ -1,17 +1,17 @@
 package com.faangx.ktp.basics
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
 import com.faangx.ktp.MiniApp
 import com.faangx.ktp.SMILE_EMOJI
+import com.faangx.ktp.comp.DynamicRowColumn
+import com.faangx.ktp.comp.HighlightedText
 
 fun StageOfLifeMiniApp(
     stageOfLife: (Int) -> String
@@ -33,32 +33,36 @@ fun StageOfLifeApp(
         age.toIntOrNull()?.run(stageOfLife) ?: SMILE_EMOJI
     }
 
-    Row (
+    DynamicRowColumn(
         Modifier.fillMaxSize()
             .padding(16.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+        horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "Person with age  ",
-            style = MaterialTheme.typography.h5
+            style = MaterialTheme.typography.titleLarge
         )
 
         OutlinedTextField(
             modifier = Modifier.width(120.dp),
+            label = { Text("Years") },
             value = age,
             onValueChange = { if (it.length <= 5) age = it },
-            textStyle = MaterialTheme.typography.h5
+            textStyle = MaterialTheme.typography.titleLarge
         )
 
         Text(
-            text = "  years is in",
-            style = MaterialTheme.typography.h5
+            text = "is in",
+            style = MaterialTheme.typography.titleLarge
         )
 
-        Text(
+        HighlightedText(
+            modifier = Modifier,
             text = " ${result.value} ",
-            style = MaterialTheme.typography.h4
+            style = MaterialTheme.typography.titleLarge
         )
     }
 }
