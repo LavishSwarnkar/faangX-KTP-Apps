8d15093572e71b45832cc590336c458e630a8daf
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-05-15 18:44:28 +0530

    adaptiveLayout : LeapYearChecker

--- src/main/kotlin/com/faangx/ktp/basics/LeapYearCheckApp.kt
+++ src/main/kotlin/com/faangx/ktp/basics/LeapYearCheckApp.kt
@@ -1,17 +1,20 @@
 package com.faangx.ktp.basics
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
 import com.faangx.ktp.MiniApp
 import com.faangx.ktp.SMILE_EMOJI
+import com.faangx.ktp.comp.DynamicRowColumn
+import com.faangx.ktp.comp.HighlightedText
 
 fun LeapYearCheckerMiniApp(
     isLeapYear: (Int) -> Boolean
@@ -35,32 +38,30 @@ fun LeapYearChecker(
         if (leap) "a Leap year" else "NOT a Leap year"
     }
 
-    Row (
+    DynamicRowColumn(
         Modifier.fillMaxSize()
             .padding(16.dp),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
+        horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Year  ",
-            style = MaterialTheme.typography.h5
-        )
-
         OutlinedTextField(
             modifier = Modifier.width(100.dp),
+            label = { Text("Year") },
             value = year,
             onValueChange = { if (it.length <= 4) year = it },
-            textStyle = MaterialTheme.typography.h5
+            textStyle = MaterialTheme.typography.titleLarge
         )
 
         Text(
-            text = "  =",
-            style = MaterialTheme.typography.h5
+            text = "is",
+            style = MaterialTheme.typography.titleLarge
         )
 
-        Text(
-            text = " ${result.value} ",
-            style = MaterialTheme.typography.h4
+        HighlightedText(
+            text = result.value,
+            style = MaterialTheme.typography.titleLarge
         )
     }
 }
