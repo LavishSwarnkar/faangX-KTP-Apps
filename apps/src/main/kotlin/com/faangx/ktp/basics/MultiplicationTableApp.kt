1b56f4d413b1e706845d32a0b06530d428c22357
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-05-22 17:01:58 +0530

    adaptiveLayout : MultiplicationTableApp

--- apps/src/main/kotlin/com/faangx/ktp/basics/MultiplicationTableApp.kt
+++ apps/src/main/kotlin/com/faangx/ktp/basics/MultiplicationTableApp.kt
@@ -3,14 +3,16 @@ package com.faangx.ktp.basics
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.faangx.ktp.MiniApp
+import com.faangx.ktp.comp.DynamicRowColumn
 import com.faangx.ktp.util.captureStdOutput
 import ksp.MiniApp
 import java.io.ByteArrayOutputStream
@@ -67,61 +69,54 @@ private fun Content(
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
     ) {
 
-        Row (
+        DynamicRowColumn (
             Modifier.fillMaxWidth()
                 .padding(16.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
+            horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterVertically)
         ) {
             Text(
-                text = "Multiplication Table of  ",
-                style = MaterialTheme.typography.h5
+                text = "Multiplication Table of",
+                style = MaterialTheme.typography.titleLarge
             )
 
             OutlinedTextField(
                 modifier = Modifier.width(120.dp),
                 value = num,
+                label = { Text("No") },
                 onValueChange = { if (it.length <= 5) num = it },
-                textStyle = MaterialTheme.typography.h5
+                textStyle = MaterialTheme.typography.titleLarge.copy(textAlign = TextAlign.Center)
             )
 
             if (customizeEndPoints) {
-                Text(
-                    text = "  from  ",
-                    style = MaterialTheme.typography.h5
-                )
+                Row {
+                    OutlinedTextField(
+                        modifier = Modifier.width(80.dp),
+                        value = start,
+                        label = { Text("From") },
+                        onValueChange = { if (it.length <= 3) start = it },
+                        textStyle = MaterialTheme.typography.titleLarge.copy(textAlign = TextAlign.Center)
+                    )
 
-                OutlinedTextField(
-                    modifier = Modifier.width(80.dp),
-                    value = start,
-                    onValueChange = { if (it.length <= 3) start = it },
-                    textStyle = MaterialTheme.typography.h5
-                )
-
-                Text(
-                    text = "  to  ",
-                    style = MaterialTheme.typography.h5
-                )
-
-                OutlinedTextField(
-                    modifier = Modifier.width(80.dp),
-                    value = end,
-                    onValueChange = { if (it.length <= 3) end = it },
-                    textStyle = MaterialTheme.typography.h5
-                )
+                    OutlinedTextField(
+                        modifier = Modifier.padding(start = 12.dp)
+                            .width(80.dp),
+                        value = end,
+                        label = { Text("To") },
+                        onValueChange = { if (it.length <= 3) end = it },
+                        textStyle = MaterialTheme.typography.titleLarge.copy(textAlign = TextAlign.Center)
+                    )
+                }
             }
-
-            Text(
-                text = " : ",
-                style = MaterialTheme.typography.h4
-            )
         }
 
         LazyColumn {
             items(result.value) {
                 Text(
                     text = it,
-                    style = MaterialTheme.typography.h6
+                    style = MaterialTheme.typography.titleLarge
                 )
             }
         }
