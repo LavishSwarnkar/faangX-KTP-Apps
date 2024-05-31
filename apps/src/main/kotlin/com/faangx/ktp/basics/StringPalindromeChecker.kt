92c4684cf35de6dcf9b2d70a04c6b0b0a22b84d2
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-05-31 10:21:35 +0530

    fix StringPalindromeChecker for practice

--- apps/src/main/kotlin/com/faangx/ktp/basics/StringPalindromeChecker.kt
+++ apps/src/main/kotlin/com/faangx/ktp/basics/StringPalindromeChecker.kt
@@ -2,37 +2,25 @@ package com.faangx.ktp.basics
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.VerticalDivider
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.faangx.ktp.LIGHT_GREEN
-import com.faangx.ktp.MiniApp
 import com.streamliners.compose.comp.select.LabelledCheckBox
+import ksp.MiniApp
 
-fun StringPalindromeCheckerMiniApp(
-    reverseStr: (String) -> String,
-    isPalindrome: (String, Boolean) -> Boolean
-) {
-    MiniApp(
-        title = "String Palindrome Checker",
-        composable = {
-            StringPalindromeChecker(reverseStr, isPalindrome)
-        }
-    )
-}
-
+@MiniApp(
+    "String Palindrome Checker",
+    "ProgrammingFundamentals/Ep4/StringPalindrome",
+    "str; str, ignoreCase"
+)
 @Composable
 fun StringPalindromeChecker(
     reverseStr: (String) -> String,
@@ -52,17 +40,18 @@ fun StringPalindromeChecker(
 
         Row(
             modifier = Modifier.height(IntrinsicSize.Min)
+                .widthIn(max = 600.dp)
         ) {
             OutlinedTextField(
-                modifier = Modifier.width(280.dp),
+                modifier = Modifier.weight(1f),
                 value = str,
                 onValueChange = { if (it.length <= 15) str = it },
-                textStyle = MaterialTheme.typography.h5.copy(
+                textStyle = MaterialTheme.typography.titleLarge.copy(
                     textAlign = TextAlign.End
                 ),
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    backgroundColor = if (palindrome.value)
-                        LIGHT_GREEN else Color.Transparent
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = if (palindrome.value) LIGHT_GREEN else Color.Transparent,
+                    unfocusedContainerColor = if (palindrome.value) LIGHT_GREEN else Color.Transparent
                 )
             )
 
@@ -72,14 +61,14 @@ fun StringPalindromeChecker(
             )
 
             OutlinedTextField(
-                modifier = Modifier.width(280.dp),
+                modifier = Modifier.weight(1f),
                 value = reverse.value,
                 readOnly = true,
                 onValueChange = { },
-                textStyle = MaterialTheme.typography.h5,
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    backgroundColor = if (palindrome.value)
-                        LIGHT_GREEN else Color.Transparent
+                textStyle = MaterialTheme.typography.titleLarge,
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = if (palindrome.value) LIGHT_GREEN else Color.Transparent,
+                    unfocusedContainerColor = if (palindrome.value) LIGHT_GREEN else Color.Transparent
                 )
             )
         }
