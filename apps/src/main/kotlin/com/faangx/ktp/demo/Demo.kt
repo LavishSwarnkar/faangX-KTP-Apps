5d69fec31ce3c91c69710b6787a83b3ffc1e4aa9
Lavish Swarnkar <lavishswarnkar@gmail.com>
2024-06-30 07:34:19 +0530

    remove demos

--- apps/src/main/kotlin/com/faangx/ktp/demo/Demo.kt
+++
@@ -1,9 +0,0 @@
-package com.faangx.ktp.demo
-
-import com.faangx.ktp.basics.FactorCalculatorMiniApp
-import com.faangx.ktp.basics.FactorCalculator_MobileMiniApp
-import com.faangx.ktp.basics.FactorialCalculatorAppDemo
-
-fun main() {
-    FactorialCalculatorAppDemo()
-}
